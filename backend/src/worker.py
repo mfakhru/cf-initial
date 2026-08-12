c84944dca58b98653a2e8d3442334589e3eec92f
Muhammad Fakhrurrozi Sutisna <fakhru2904@gmail.com>
2026-08-12 14:44:51 +0700

    fix: add CORS middleware to allow Pages frontend requests

--- backend/src/worker.py
+++ backend/src/worker.py
@@ -1,9 +1,17 @@
 import asgi
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from workers import WorkerEntrypoint
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
